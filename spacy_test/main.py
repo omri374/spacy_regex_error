be140279d56434b5aed7da481515c145ba5dd465
omri374 <omri374@users.noreply.github.com>
2019-04-17 10:57:20 +0300

    fixed version

--- spacy_test/main.py
+++ spacy_test/main.py
@@ -4,7 +4,9 @@ import regex as re
 if __name__ == "__main__":
 
     nlp = spacy.load("en_core_web_lg")
-    doc = nlp(u"Apple is looking at buying U.K. startup for $1 billion")
+    doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 
     for ent in doc.ents:
         print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+    print(re.match("([^\s]+)", "Apple is looking at buying U.K. startup for $1 billion"))
